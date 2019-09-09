5424ce835a9c7c43509dd12360c94461d19766eb
Hany <hanyalbouz@gmail.com>
2019-09-09 15:21:20 -0400

    created window

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-print("hello")
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+import sys
+
+
+class Main(QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.setGeometry(400, 100, 1200, 800)
+        self.setWindowTitle("Plotted graph")
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = Main()
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
